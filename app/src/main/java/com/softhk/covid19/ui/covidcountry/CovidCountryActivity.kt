8f94b509f47ecc8e77a73a04d2a7cbdfc56ef749
Jorge Luis Guzmán Saldaña <jorgeguzman2210@gmail.com>
2020-03-26 00:02:09 -0400

    Refractory main activity, adding viewBinding

--- app/src/main/java/com/softhk/covid19/ui/covidcountry/CovidCountryActivity.kt
+++ app/src/main/java/com/softhk/covid19/ui/covidcountry/CovidCountryActivity.kt
@@ -6,13 +6,12 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.softhk.covid19.AboutMeActivity
 import com.softhk.covid19.R
+import com.softhk.covid19.databinding.ActivityCovidCountryBinding
 import com.softhk.covid19.di.CovidApp
 import com.softhk.covid19.domain.CovidCountry
-import kotlinx.android.synthetic.main.activity_covid_country.*
 import javax.inject.Inject
 
 class CovidCountryActivity : AppCompatActivity(), CovidCountryContract.View,
@@ -21,21 +20,22 @@ class CovidCountryActivity : AppCompatActivity(), CovidCountryContract.View,
     @Inject
     lateinit var presenter: CovidCountryContract.Presenter
 
-    private val swipeRefreshRv by lazy { swipeRefeshRecyclerView }
-    private val viewError by lazy { errorViewLayout }
-    private val errorButton by lazy { errorButtonView }
     private val adapter by lazy { CovidCountryAdapter(this) }
-    private val recyclerView by lazy { covidCountryRecyclerView }
-    private val covid19Toolbar by lazy { covidToolbar }
-    private val showLoadingRetryDataAnim by lazy { showLoadingRetryDataAnimation }
-    private val showLostNetworkAnim by lazy{showLostNetworkAnimation}
+
+    private lateinit var binding: ActivityCovidCountryBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_covid_country)
+
+        binding = ActivityCovidCountryBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
         setUp()
-        errorButton.setOnClickListener(this)
+
+        binding.errorButtonView.setOnClickListener(this)
     }
 
     override fun setCovidCountryList(covidCountryList: List<CovidCountry>) {
@@ -43,33 +43,33 @@ class CovidCountryActivity : AppCompatActivity(), CovidCountryContract.View,
     }
 
     override fun hideSwipeRefresh(hide: Boolean) {
-        swipeRefreshRv.isRefreshing = hide
+        binding.swipeRefeshRecyclerView.isRefreshing = hide
     }
 
     override fun showSwipeRefreshLayoutWithRecyclerView() {
-        swipeRefreshRv.visibility = View.VISIBLE
+        binding.swipeRefeshRecyclerView.visibility = View.VISIBLE
     }
 
     override fun hideSwipeRefreshLayoutWithRecyclerView() {
-        swipeRefreshRv.visibility = View.INVISIBLE
+        binding.swipeRefeshRecyclerView.visibility = View.INVISIBLE
     }
 
     override fun showErrorView() {
-        viewError.visibility = View.VISIBLE
+        binding.errorViewLayout.visibility = View.VISIBLE
     }
 
     override fun hideErrorView() {
-        viewError.visibility = View.INVISIBLE
+        binding.errorViewLayout.visibility = View.INVISIBLE
     }
 
     override fun showLoadingRetryA() {
-        showLostNetworkAnim.visibility = View.VISIBLE
-        showLoadingRetryDataAnim.visibility = View.GONE
+        binding.showLostNetworkAnimation.visibility = View.VISIBLE
+        binding.showLoadingRetryDataAnimation.visibility = View.GONE
     }
 
     override fun showLostNetworkRetryA() {
-        showLoadingRetryDataAnim.visibility = View.VISIBLE
-        showLostNetworkAnim.visibility = View.GONE
+        binding.showLoadingRetryDataAnimation.visibility = View.VISIBLE
+        binding.showLostNetworkAnimation.visibility = View.GONE
     }
 
     override fun onRefresh() {
@@ -105,22 +105,25 @@ class CovidCountryActivity : AppCompatActivity(), CovidCountryContract.View,
         //Set Dagger to activity
         (application as CovidApp).getCovidComponent().inject(this)
 
-        //Set event to SwipeRefreshRv
-        swipeRefreshRv.setOnRefreshListener(this)
-
-        //Set Toolbar to Activity
-        setSupportActionBar(covid19Toolbar)
-
-        //Set Adapter to RecyclerView
-        recyclerView.adapter = adapter
-
         //Attach Activity to Presenter
         presenter.attach(this)
+
+        with(binding) {
+            //Set event to SwipeRefreshRv
+            swipeRefeshRecyclerView.setOnRefreshListener(this@CovidCountryActivity)
+
+            //Set Toolbar to Activity
+            setSupportActionBar(covidToolbar)
+
+            //Set Adapter to RecyclerView
+            covidCountryRecyclerView.adapter = adapter
+        }
+
 
     }
 
     private fun loadData() {
-        swipeRefreshRv.isRefreshing = true
+        binding.swipeRefeshRecyclerView.isRefreshing = true
         presenter.loadDefaultCovidCountriesList()
     }
 }
